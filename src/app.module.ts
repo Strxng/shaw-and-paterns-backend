38a1140df4776f5ede9312ba41212a7cfd52106b
Leo B <leonardo_picinin02@hotmail.com>
2023-05-08 16:11:50 -0300

    create health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProvidersModule } from './providers/providers.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), ProvidersModule, UsersModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
