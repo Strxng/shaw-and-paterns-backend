1c68e499d4058e916d417829d79101e6fd9eaae3
Leo B <leonardo_picinin02@hotmail.com>
2023-05-08 16:41:54 -0300

    /

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('')
 export class AppController {
+  @Get('/')
+  async home() {
+    return true;
+  }
+
   @Get('/healthz')
   async healthCheck() {
     return true;
